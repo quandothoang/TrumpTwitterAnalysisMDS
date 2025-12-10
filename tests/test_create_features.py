87da371762d95f6e279342666bf481e8fdb7f2b0
mailysg8 <mai.guedon@gmail.com>
2025-12-10 14:29:36 -0800

    Added test for normal case

--- tests/test_create_features.py
+++ tests/test_create_features.py
@@ -5,6 +5,7 @@ import pandas as pd
 import sys
 sys.path.append(os.path.join(os.path.dirname(__file__), '..'))
 
+from src.data_utils import create_features
 
 # Test case 1 : 3 normal tweets and times
 
@@ -15,15 +16,13 @@ test_case_1_input = pd.DataFrame({
                       'Il fait toujours beau au dessus des nuages. Jécouterai sous la pluie la symphonie des éclairs! Ses cris et ses larmes qui lui faisait tant///...']
 })
 print(test_case_1_input)
-test_case_1_output = pd.DataFrame({
-    'Date & Time' : [pd.Timestamp(2012,12,12,12,22,14),pd.Timestamp(2012,7,14,15,26,17), pd.Timestamp(2011,3,20,1,3,5)],
-    'Tweet Text' : ['I am a concerned citizen!', 
-                    'There is nothing to worry about, we have heard your complaints and decided to remove them! Be afraid...',
-                    'Il fait toujours beau au dessus des nuages. Jécouterai sous la pluie la symphonie des éclairs! Ses cris et ses larmes qui lui faisait tant///...'],
-    'length' : [len('I am a concerned citizen!'), 
-                len('There is nothing to worry about, we have heard your complaints and decided to remove them! Be afraid...'),
-                len('Il fait toujours beau au dessus des nuages. Jécouterai sous la pluie la symphonie des éclairs! Ses cris et ses larmes qui lui faisait tant///...')],
-    'hour'  :  [pd.Timestamp(2012,12,12,12,22,14).hour,pd.Timestamp(2012,7,14,15,26,17).hour, pd.Timestamp(2011,3,20,1,3,5).hour],
+test_case_1_output = pd.concat([test_case_1_input.copy().reset_index(), pd.DataFrame({
+    'length' : [len(test_case_1_input.loc[0,'Tweet Text']), 
+                len(test_case_1_input.loc[1,'Tweet Text']),
+                len(test_case_1_input.loc[2,'Tweet Text'])],
+    'hour'  :  [int(test_case_1_input.loc[0,'Date & Time'].hour), 
+                int(test_case_1_input.loc[1,'Date & Time'].hour),
+                int(test_case_1_input.loc[2,'Date & Time'].hour)],
     'weekday' : [pd.Timestamp(2012,12,12,12,22,14).weekday(),pd.Timestamp(2012,7,14,15,26,17).weekday(), pd.Timestamp(2011,3,20,1,3,5).weekday()],
     'year' : [pd.Timestamp(2012,12,12,12,22,14).year,pd.Timestamp(2012,7,14,15,26,17).year, pd.Timestamp(2011,3,20,1,3,5).year],
     'month' : [pd.Timestamp(2012,12,12,12,22,14).month,pd.Timestamp(2012,7,14,15,26,17).month, pd.Timestamp(2011,3,20,1,3,5).month],
@@ -33,22 +32,23 @@ test_case_1_output = pd.DataFrame({
     'avg_word_length' : [4.2, 4.8, 4.8],
     'word_count' : [5, len('There is nothing to worry about, we have heard your complaints and decided to remove them! Be afraid...'.split()), len('Il fait toujours beau au dessus des nuages. Jécouterai sous la pluie la symphonie des éclairs! Ses cris et ses larmes qui lui faisait tant///...'.split())],
     'punctuation_count' : [1, 5, 8]
-})
+})], axis=1)
+
 
 print(test_case_1_output)
 
-# Edge case 1 : empty string
+# Edge case 1 : single letter tweet
 
 edge_case_1_input = pd.DataFrame({
     'Date & Time' : [pd.Timestamp(2012,12,12,12,22,14)],
-    'Tweet Text' : ['']
+    'Tweet Text' : ['I']
 })
-print(edge_case_1_input)
+#print(edge_case_1_input)
 
 edge_case_1_output = pd.DataFrame({
     'Date & Time' : [pd.Timestamp(2012,12,12,12,22,14)],
-    'Tweet Text' : [''],
-    'length':[0], 
+    'Tweet Text' : ['I'],
+    'length':[1], 
     'hour' : [12],
     'weekday' : [2],
     'year' : [2012],
@@ -56,11 +56,78 @@ edge_case_1_output = pd.DataFrame({
     'day' : [12],
     'season' : ['autumn'],
     'time_of_day' : ['daytime'],
-    'avg_word_length' : [0],
-    'word_count' : [0],
+    'avg_word_length' : [1],
+    'word_count' : [1],
     'punctuation_count' : [0]
 })
 
-print(edge_case_1_output)
-from src.data_utils import create_features
-create_features(edge_case_1_input)
+# Edge case 2 : only punctuation
+
+edge_case_2_input = pd.DataFrame({
+    'Date & Time' : [pd.Timestamp(2012,12,12,12,22,14)],
+    'Tweet Text' : ['...///...^55']
+})
+#print(edge_case_2_input)
+
+edge_case_1_output = pd.DataFrame({
+    'Date & Time' : [pd.Timestamp(2012,12,12,12,22,14)],
+    'Tweet Text' : ['I'],
+    'length':[1], 
+    'hour' : [12],
+    'weekday' : [2],
+    'year' : [2012],
+    'month' : [12],
+    'day' : [12],
+    'season' : ['autumn'],
+    'time_of_day' : ['daytime'],
+    'avg_word_length' : [1],
+    'word_count' : [1],
+    'punctuation_count' : [0]
+})
+
+
+# Error case 1 : empty string
+
+error_case_1_input = pd.DataFrame({
+    'Date & Time' : [pd.Timestamp(2012,12,12,12,22,14)],
+    'Tweet Text' : ['I am a concerned citizen!']
+})
+#print(error_case_1_input)
+
+# Error case 2 : input not dataframe
+
+error_case_2_input = {
+    'Date & Time' : [pd.Timestamp(2012,12,12,12,22,14)],
+    'Tweet Text' : ['']
+}
+#print(error_case_2_input)
+
+# Error case 3 : no Date & time or Tweet Text column
+error_case_3_input = pd.DataFrame({
+    'Date' : [pd.Timestamp(2012,12,12,12,22,14)],
+    'Tweet' : ['I am a concerned citizen!']
+})
+#print(error_case_3_input)
+
+# Error case 4 :  Date & time  not timestamp or tweet text not string
+error_case_4_input = pd.DataFrame({
+    'Date & Time'  : [pd.Timestamp(2012,12,12,12,22,14)],
+    'Tweet Text' : ['I am a concerned citizen!']
+})
+#print(error_case_4_input)
+
+# Tests :
+def create_features_test_normal():
+    """ Tests to ensure create_features function correctly adds features."""
+    results = create_features(test_case_1_input)
+
+    # Verify return type 
+    assert isinstance(results, pd.DataFrame)
+
+    # Verify that the output dataframe is as expected
+    pd.testing.assert_frame_equal(test_case_1_output, results, check_dtype = False)
+
+
+print(create_features_test_normal())
+
+print()
